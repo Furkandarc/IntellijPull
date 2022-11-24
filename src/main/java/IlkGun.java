637edeae635eb2a9faf913cc008be15df1afb873
Furkan Darıcı <fdrc2002@gmail.com>
2022-11-24 22:51:04 +0300

    V3

--- src/main/java/IlkGun.java
+++ src/main/java/IlkGun.java
@@ -3,5 +3,6 @@ public class IlkGun {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Git Github ilk deneme");
+        System.out.println("Git Github ikinci deneme");
     }
 }
